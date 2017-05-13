72ed88fd0d3518b6d7940f4754ba4d0a76b1ec2d
Anders Wood <anderswood@gmail.com>
2017-05-13 17:40:28 -0600

    css start

--- javascript.js
+++ javascript.js
@@ -1,4 +1,23 @@
-const firstScreenClick = document.querySelector('first-screen')
-firstScreenClick.addEventListener('.click', () => {
-  
+// const firstScreen = document.querySelector('body');
+const backArrow = document.querySelector('.back-arrow');
+const screen1 = document.querySelector('.screen1-container');
+const screen2 = document.querySelector('.screen2-container');
+
+screen1.addEventListener('click', () => {
+  if(screen1.className.indexOf('inactive') === -1) {
+    toggleClasses();
+  }
 })
+
+backArrow.addEventListener('click', () => {
+  if (screen2.className.indexOf('inactive') === -1) {
+    toggleClasses();
+  }
+})
+
+const toggleClasses = () => {
+  screen1.classList.toggle('active')
+  screen1.classList.toggle('inactive')
+  screen2.classList.toggle('active')
+  screen2.classList.toggle('inactive')
+}
